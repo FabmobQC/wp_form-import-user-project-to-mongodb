94fccdd6db6e75a1bced1e707890a275d67b598b
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2022-10-17 17:14:48 +0000

    Updated script

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Description
 
 Tool to import Wordpress WP Form field and insert it in the MongoDB
+
+# Usage
+
+Create an .env file following this example
+
+```
+{
+        "databases": {
+                "mongodb": {
+
+                        "url": MONGODB_CONNECTION_STRING
+                },
+                "mysql": {
+                        "host": HOST,
+                        "user": USERNAME,
+                        "password" : PASSWORD,
+                        "dbname": DATABASE_NAME
+                }
+        }
+}
+```
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Script to import field name from WP Form on Wordpress Mysql DB to MongoDB
 
 import json
+import uuid
 import mysql.connector
 from pymongo import MongoClient
 
@@ -24,11 +25,37 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT COUNT(wp_tc2c36s7fq_wpforms_entry_fields.id) FROM `wp_tc2c36s7fq_wpforms_entry_fields` WHERE wp_tc2c36s7fq_wpforms_entry_fields.form_id = 25100")
-
-myresult = mycursor.fetchall()
+#mycursor.execute("SELECT COUNT(wp_tc2c36s7fq_wpforms_entry_fields.id) FROM `wp_tc2c36s7fq_wpforms_entry_fields` WHERE wp_tc2c36s7fq_wpforms_entry_fields.form_id = 25100")
 
 
-for x in myresult:
-  print(x)
+def update_user_project(mysql_c,  mongodb_c, form_id, uuid_field_id, project_field_id):
+
+    mysql_c.execute("SELECT * FROM `wp_tc2c36s7fq_wpforms_entries` WHERE form_id={}".format(form_id))
+    entries = mycursor.fetchall()
+
+    for entrie in entries:
+        user_uuid = ""
+        project = ""
+
+
+        try:
+            user_uuid = json.loads(entrie[8])[uuid_field_id]['value']
+        except:
+            print("Error retrieving user id for entry {}".format(str(entrie)))
+        
+        try:
+            if user_uuid != "":
+                project = json.loads(entrie[8])[project_field_id]['value']
+                if user_uuid != "" and project != "":
+                    mongodb_c.Stage_Profiles.update({"user_id": uuid.UUID(user_uuid)}, {"$set": { "project_name": project}})
+        except:
+            print("Error retrieving user {} project".format(str(user_uuid)))
+
+        if user_uuid != "" and project != "":
+            print("User {} has project {}".format(user_uuid, project))
+
+
+
+update_user_project(mycursor, mongodb, "25100", "14", "28")
+update_user_project(mycursor, mongodb, "25278", "14", "24")
 
